b207d8393c5219f7e9755bf9ae5e68089e8e8c0c
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2024-11-29 15:04:52 -0600

    Added files to the backend

---
+++ backend/app/database.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# Load values from .env file
+load_dotenv()
+
+# Access the MongoDB_URI variable
+MONGO_URI = os.getenv("MONGO_URI")
+
+# This file defines the connection to our database, in this case the MongoDB Cluster.
+mongo_conn = MongoClient(MONGO_URI)
+
+db = mongo_conn["leafy_factory"]
+work_orders_coll = db["work_orders"]
