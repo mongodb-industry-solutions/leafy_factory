b63fd7a4093509fb3289040d9632815a25599163
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2024-12-17 16:10:18 -0600

    Modified the Axios version, since latest version throws an error.

--- backend/app/models/machines.py
+++ backend/app/models/machines.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel, Field
 import datetime
 
 class MachineHeartbeat(BaseModel):
+    factory_id: str = Field(..., description = "Factory ID")
+    production_line_id: str = Field(..., description = "Production Line ID")
     machine_id: str = Field(..., description = "Machine ID")
     vibration: float = Field(..., description = "Machine's vibration value, this is currently generated randomly on the Lambda Function")
     temperature: float = Field(..., description = "Machine's temperature value, this is currently generated randomly on the Lambda Function")
--- backend/app/routes/machines.py
+++ backend/app/routes/machines.py
@@ -2,22 +2,47 @@ from fastapi import APIRouter, HTTPException, status
 from app.database import machine_data_coll
 from app.models.machines import MachineHeartbeat
 from fastapi.responses import JSONResponse
+from datetime import datetime, timedelta
 
 router = APIRouter()
+
+def get_bucket_time(timestamp):
+    # list_time will store the start_date and end_date
+    list_time = []
+    start_date = timestamp.replace(minute=0, second=0, microsecond=0)
+    end_date = start_date + timedelta(hours=1, seconds=-1)
+    
+    list_time.append(start_date)
+    list_time.append(end_date)
+
+    return list_time
 
 @router.post("/machines/heartbeat")
 async def receive_heartbeat(data: MachineHeartbeat):
     """This endpoint inserts the heartbeat data sent from Lambda to MongoDB database"""
-    
     try:
         heartbeat_record = {
+            "factory_id": data.factory_id,
+            "production_line_id": data.production_line_id,
             "machine_id": data.machine_id,
             "vibration": data.vibration,
             "temperature": data.temperature,
             "timestamp": data.timestamp
         }
 
-        print(heartbeat_record)
+        print(heartbeat_record['timestamp'])
+        
+        # list_time[0] includes start_date
+        # list_time[1] includes end_date
+        list_time = get_bucket_time(heartbeat_record['timestamp'])
+
+        # Bucket filter query
+        filter_query = {
+            "start_date": list_time[0],
+            "end_date": list_time[1],
+            "factory.factory_id": heartbeat_record["factory_id"],
+            "factory.production_lines": heartbeat_record["factory_id"]
+        }
 
         return JSONResponse(
             status_code=status.HTTP_201_CREATED,
