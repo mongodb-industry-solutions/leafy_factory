b310e1aabdbaecdd01facbf69a2d3d164616c22d
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2024-12-23 15:29:15 -0600

    Modified the API call to update work orders, now updates the work orders from the SQL database

--- backend/app/routes/work_orders.py
+++ backend/app/routes/work_orders.py
@@ -68,16 +68,40 @@ def create_work_order(work_order: WorkOrder):
                             )
                             VALUES (?,?,?,?,?,?)
                             """
+        
+        insert_cost_work_order ="""
+                                INSERT INTO product_cost (
+                                    raw_material_cost_per_product,
+                                    overhead_per_product,
+                                    total_cost_per_product,
+                                    work_id
+                                )
+                                VALUES (?,?,?,?)
+                                """
+        
         with mariadb_conn.cursor() as db_insert_cursor:
             db_insert_cursor.execute(insert_work_order, 
-                                     (workorder_json["planned_start_date"],
-                                      workorder_json["planned_end_date"],
-                                      workorder_json["quantity"],
-                                      workorder_json["status"],
-                                      workorder_json["creation_date"],
-                                      workorder_json["product_cat_id"],
-                                      ))
+                                     (
+                                        workorder_json["planned_start_date"],
+                                        workorder_json["planned_end_date"],
+                                        workorder_json["quantity"],
+                                        workorder_json["status"],
+                                        workorder_json["creation_date"],
+                                        workorder_json["product_cat_id"],
+                                     ))
+            
             new_work_order_id = db_insert_cursor.lastrowid
+
+            db_insert_cursor.execute(insert_cost_work_order, 
+                                     (
+                                         workorder_json["cost"]["planned"]["raw_material_cost_per_product"],
+                                         workorder_json["cost"]["planned"]["overhead_per_product"],
+                                         workorder_json["cost"]["planned"]["total_cost_per_product"],
+                                         new_work_order_id,
+                                     ))
+            
+            new_work_cost_id = db_insert_cursor.lastrowid
+
             # Commit the insert operation.
             mariadb_conn.commit()
         
@@ -210,6 +234,76 @@ def get_work_order():
     #Returns a list of JSON documents (work_order_list)
     return work_order_list
 
+
+#Update work order from Created to Completed
+@router.put("/workorders/{work_id}",
+            summary="Updates the work order status from 'Created' to 'Completed'",
+            description="This endpoint updates the work_order items with status 'Created', adds the 'actual_start' and 'actual_end' and cost.actual fields, and changes the status of the work_order item",
+            responses={
+                200: {
+                    "description": "Work order updated successfully",
+                    "content": {
+                         "application/json":{
+                             "example": { "Result" : "True","Modified Count" : "1" , "Updated Document work_id" : "2" }
+                         }
+                     }
+                }
+            })
+def update_work_order(work_id: int, updated_work_order: UpdateWorkOrder):
+    if not updated_work_order.actual_start_date or not updated_work_order.actual_start_date:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="The actual start date and actual end date are required to update the status of the work_order"
+        )
+    try:
+        updated_workorder_json = updated_work_order.model_dump()
+        work_order_status = "Completed"
+
+        # Update the document, adding actual_start_date, actual_end_date and actual.final_product_cost_per_job
+        work_order_update_query = f"UPDATE work_orders SET actual_start_date = %s, actual_end_date = %s, wo_status = %s, nOk_products = %s WHERE id_work = {work_id}"
+        cost_update_query = f"UPDATE product_cost SET cost_ok_with_overhead = %s, cost_nok_with_overhead = %s, actual_total_cost = %s WHERE work_id = {work_id}"
+
+        with mariadb_conn.cursor() as db_cur:
+            db_cur.execute(work_order_update_query, (
+                updated_workorder_json["actual_start_date"],
+                updated_workorder_json["actual_end_date"],
+                work_order_status,
+                updated_workorder_json['final_product_cost_per_job']['nOk_products']
+            ),)
+
+            total_wo_updated = db_cur.rowcount
+
+            db_cur.execute(cost_update_query, (
+                updated_workorder_json['final_product_cost_per_job']['cost_ok_with_overhead'],
+                updated_workorder_json['final_product_cost_per_job']['cost_nok_with_overhead'],
+                updated_workorder_json['final_product_cost_per_job']['total_cost'],
+            ),)
+
+            total_product_cost_updated = db_cur.rowcount
+            
+            # Commit the update operations.
+            mariadb_conn.commit()
+
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content={"Result": "True", 
+                     "Modified Work Orders raws": str(total_wo_updated), 
+                     "Modified Product Cost raws": str(total_product_cost_updated), 
+                     "Updated Document work_id": str(work_id)}
+        )
+             
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to update work order: {str(e)}"
+        )
+
+
+#########################################################
+#########################################################
+###### THIS API UPDATES A WORKORDER FROM MONGODB ########
+#########################################################
+#########################################################
 
 #Update work order from Created to Completed
 @router.put("/workorders/{work_id}",
