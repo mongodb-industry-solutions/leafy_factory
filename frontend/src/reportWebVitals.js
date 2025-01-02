d11ca97f86f2c94b5d1a6c1df67058e3075ca2e4
Romina Carranza <romina.lopezcarranza@mongodb.com>
2025-01-02 14:32:42 -0600

    reboot of react app to avoid dedupe command

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,7 +1,4 @@
-import 'react-refresh/runtime';
-
 const reportWebVitals = onPerfEntry => {
-
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
--- frontend/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export interface WorkOrder {
-    id: string;
-    productName: string;
-    quantity: number;
-    status: 'created' | 'completed';
-    createdAt: string;
-    dueDate: string;
-  }
