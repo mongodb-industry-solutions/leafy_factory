9e9706b085fe91da00cb6ffbcc1b4262a44bba12
Romina Carranza <romina.lopezcarranza@mongodb.com>
2025-01-02 15:45:12 -0600

    upload of react app and folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import {Switch, BrowserRouter as Router } from 'react-router-dom'
+import Header from './components/Layout/Header';
+import Home from './components/Home';
+import workOrderState from './context/workOrders/workOrdersState';
+import PublicRoute from './components/Routes/PublicRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <workOrderState>
+
+      </workOrderState>
+    <>
   );
 }
 
