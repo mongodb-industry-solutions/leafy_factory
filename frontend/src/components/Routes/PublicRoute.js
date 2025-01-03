a9cdd27bcc08a27d7b16de1fd85c0eafc5c1f659
Romina Carranza <romina.lopezcarranza@mongodb.com>
2025-01-02 21:31:52 -0600

    update for react v6 hooks and visualization of FE

--- frontend/src/components/Routes/PublicRoute.js
+++ frontend/src/components/Routes/PublicRoute.js
@@ -1,31 +1,8 @@
-import React, { Component, useContext, useEffect, useState } from 'react';
-import { Route, Redirect } from 'react-router-dom'
-import UsersContext from './../../context/Users/UsersContext'
+import React from 'react';
+//import { Route } from 'react-router-dom';
 
 const PublicRoute = ({ component: Component, ...props }) => {
-    const usersCtx = useContext(UsersContext)
-    const {
-        authStatus,
-        tokenVerification
-    } = usersCtx
+  return <Component {...props} />;
+};
 
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        const verifyUser = async () => {
-            await tokenVerification();
-            setLoading(false)
-        }
-        verifyUser()
-    }, [authStatus])
-
-    return (
-        <Route{...props} render={(props) => {
-            if (loading) return null
-            return <Component{...props} />
-
-        }} />
-    )
-
-}
-export default PublicRoute
+export default PublicRoute;
