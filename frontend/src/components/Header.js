b2c33755a7621c1c52e0e4b927d6b9d8c1e7aae8
Romina Carranza <romina.lopezcarranza@mongodb.com>
2025-01-06 13:20:36 -0600

    Reducer, State and Context from WorkOrders

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,20 @@
+//import "./styles.css";
 import React from 'react';
+//import LeafyProvider from '@leafygreen-ui/leafygreen-provider'
+//import Stepper, { Step } from '@leafygreen-ui/stepper';
 
 function Header() {
     return (
         <header className='header'>
             <h1>Welcome to Leafy Factory Work Order Process</h1>
+            {/*<LeafyProvider>
+            <div className="sandbox">
+                <Stepper currentStep={0} maxDisplayedSteps={2}>
+                <div>Step 1</div>
+                <div>Step 2</div>
+                </Stepper>
+            </div>
+            </LeafyProvider>*/}
             <img src="https://images.contentstack.io/v3/assets/blt7151619cb9560896/blta30d0168850404a8/65fda6758f44440029c3a12a/la1a1agcxt7ppntea-logo-marks.svg" className="logo" alt="logo" />
         </header>
     );
