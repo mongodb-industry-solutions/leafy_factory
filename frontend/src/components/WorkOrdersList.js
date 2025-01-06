1df0ce6e40dc789e5ff63de3c97efae5c8f3dd9a
angie <angie.guemes@mongodb.com>
2025-01-06 14:45:45 -0600

    redux(front): add Store to manage state, test leafygreen

---
+++ frontend/src/components/WorkOrdersList.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import {
+    useDispatch, // to MODIFY the Store
+    useSelector // to ACCESS the store
+} from "react-redux";
+import axiosClient from '../config/axios';
+import ExpandableCard from '@leafygreen-ui/expandable-card';
+import Card from '@leafygreen-ui/card';
+
+import { setAllOrders } from '../redux/slices/WorkOrderslice';
+
+const WorkOrdersList = () => {
+    const dispatch = useDispatch() // dispatch to MODIFY redux state
+    const workOrders = useSelector(state => state.WorkOrders.workOrders)
+
+    useEffect(() => {
+        // load all work orders once this component gets rendered
+
+        const fetchWorkOrders = async () => {
+            try {
+                const response = await fetch('https://rickandmortyapi.com/api/character')
+                // TODO replace latter with --> axiosClient.get("/workorders/")
+                const result = await response.json(); // this line will be removed once we have Python api
+                console.log('-- getAllWorkOrders', result)
+                const myWorkOrdersList = result.results // replace with correct access to list from the /wororders/ api
+                dispatch(setAllOrders([...myWorkOrdersList]))
+            } catch (error) {
+                console.error("There was a problem with your fetch operation:", error);
+            }
+        };
+
+        fetchWorkOrders();
+    }, [])
+
+
+    return (
+        <div>
+            {
+                workOrders.map((order, index) => (
+                    <div key={index}>
+                        {/* <Card className="card-styles" as="article">
+                            This is my card component
+                        </Card>; */}
+                        <div class="leafygreen-ui-1lu17q2 card-styles card">
+
+                        {order.id} - {order.name}
+                        </div>
+                        {/* <ExpandableCard
+                            title='hola'
+                            description="..."
+                            flagText="optional"
+                        >
+                            {order.id} - {order.created}
+                        </ExpandableCard>; */}
+
+                    </div>
+                ))
+            }
+
+        </div>
+    )
+}
+
+export default WorkOrdersList
