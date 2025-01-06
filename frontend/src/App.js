933b4fbd1d35a1a4ed2f1f72d6917b27714742fd
Romina Carranza <romina.lopezcarranza@mongodb.com>
2025-01-06 13:32:33 -0600

    push for changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,15 @@ import Navbar from './components/Navbar';
 import ArchOverview from './components/Overview';
 import { Route, Routes } from 'react-router-dom';
 import ErrorPage from './components/ErrorPage'
+import WorkOrderState from './context/workOrders/workOrdersState';
+
 
 const text = <h2>Work Orders History and to be submitted here:</h2>;
 
 function App() {
   return (
     <div className="App">
+       <WorkOrderState />
       <Navbar />
       <Header />
 
