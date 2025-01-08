f09e06589d239f3046f4d9b3c964bd7aaafcde39
Romina Carranza <romina.lopezcarranza@mongodb.com>
2025-01-08 14:51:56 -0600

    Update for rendering Navbar.js WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
-//App.js
 import './App.css';
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
-import ArchOverview from './components/Overview';
-import { Route, Routes } from 'react-router-dom';
-import ErrorPage from './components/ErrorPage'
-import Factory from './components/Factory';
+//import ArchOverview from './components/Overview';
+//import { Route, Routes } from 'react-router-dom';
+//import ErrorPage from './components/ErrorPage'
+//import Factory from './components/Factory';
 
 
 function App() {
@@ -15,13 +14,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Header />
-      
-      <Factory></Factory>
 
-      <Routes>
-        <Route path="/overview" element={<ArchOverview />} />
-        <Route component={ErrorPage} />
-      </Routes>
     </div>
   );
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,26 +1,47 @@
-//Navbar.js
-import React from 'react';
-import navImage from '../images/factory.png';
-import { Link,} from 'react-router-dom';
-//import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useState} from "react";
+import navImage from "../images/factory.png";
+//import { Link } from "react-router-dom";
+import { Tab, Tabs } from "react-bootstrap";
+import ArchOverview from './Overview'; 
+import Factory from './Factory';
+
 
 function Navbar() {
+    const [key, setKey] = useState('workorders'); // Manage active tab state
+
     return (
         <nav className="navbar">
-        {/*<Routes>*/}
-        <div>
-            <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
-                <img id="nav-img" src={navImage} alt="Leafy Factory Logo" />
-                <h1 className="navtext">Leafy Factory</h1>
-          </Link>
-        </div>
-
-        <div className="navbar-overview">
-            <Link to="/overview" className="overview-link">Demo Overview</Link>
-        </div>
-        {/*</Routes>*/}
+          {/* Logo and Title */}
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <img id="nav-img" src={navImage} alt="Leafy Factory Logo" style={{ height: '50px', marginRight: '10px' }} />
+            <h1 className="navtext">Leafy Factory</h1>
+          </div>
+    
+          {/* Tabs Container */}
+          <div className="w-100 d-flex justify-content-center mt-3">
+            <Tabs
+              id="navbar-tabs"
+              activeKey={key}
+              onSelect={(k) => setKey(k)} 
+              className="mb-3"
+              justify
+            >
+              <Tab eventKey="workorders" title="Work Orders">
+                <Factory />
+              </Tab>
+              <Tab eventKey="jobs" title="Jobs">
+                <h3>Jobs Tab</h3>
+              </Tab>
+              <Tab eventKey="shopfloor" title="Shopfloor Simulator" disabled>
+                <span>Shopfloor Simulator (Disabled)</span>
+              </Tab>
+              <Tab eventKey="overview" title="Demo Overview">
+                <ArchOverview />
+              </Tab>
+            </Tabs>
+          </div>
         </nav>
-  );
+      );
 }
 
 export default Navbar;
