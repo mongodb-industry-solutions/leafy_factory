982ff3624b4b2517ebd5b63683341777f1f40d78
Romina Carranza <romina.lopezcarranza@mongodb.com>
2025-01-08 21:29:04 -0600

    Bug fixed inside Navbar, loading Work Orders and DemoOverviewArch explanation

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,45 +1,49 @@
-import React, { useState} from "react";
+import React from "react";
 import navImage from "../images/factory.png";
-//import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Tab, Tabs } from "react-bootstrap";
-import ArchOverview from './Overview'; 
-import Factory from './Factory';
-
+//import ArchOverview from "./Overview";
+//import Factory from "./Factory";
 
 function Navbar() {
-    const [key, setKey] = useState('workorders'); // Manage active tab state
+  const navigate = useNavigate();
+  const handleTabSelector = key => {
+    navigate(`/${key}`);
+  };
 
-    return (
-        <nav className="navbar">
-          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <img id="nav-img" src={navImage} alt="Leafy Factory Logo" style={{ height: '50px', marginRight: '10px' }} />
-            <h1 className="navtext">Leafy Factory</h1>
-          </div>
-    
-          <div className="w-100 d-flex justify-content-center mt-3">
-            <Tabs
-              id="navbar-tabs"
-              activeKey={key}
-              onSelect={(k) => setKey(k)} 
-              className="mb-3"
-              justify
-            >
-              <Tab eventKey="workorders" title="Work Orders">
-                <Factory />
-              </Tab>
-              <Tab eventKey="jobs" title="Jobs">
-                <h3>Jobs Tab</h3>
-              </Tab>
-              <Tab eventKey="shopfloor" title="Shopfloor Simulator" disabled>
-                <span>Shopfloor Simulator (Disabled)</span>
-              </Tab>
-              <Tab eventKey="overview" title="Demo Overview">
-                <ArchOverview />
-              </Tab>
-            </Tabs>
-          </div>
-        </nav>
-      );
+  return (
+    <nav className="navbar">
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <img
+          id="nav-img"
+          src={navImage}
+          alt="Leafy Factory Logo"
+          style={{ height: "50px", marginRight: "10px" }}
+        />
+        <h1 className="navtext">Leafy Factory</h1>
+      </div>
+
+      <div className="w-100 d-flex justify-content-center mt-3">
+        <Tabs
+          id="navbar-tabs"
+          onSelect={handleTabSelector}
+          className="mb-3"
+          justify
+        >
+          <Tab eventKey="workorders" title="Work Orders" />
+          <Tab eventKey="jobs" title="Jobs" />
+          <Tab eventKey="shopfloor" title="Shopfloor Simulator" disabled />
+          <Tab eventKey="overview" title="Demo Overview" />
+        </Tabs>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
