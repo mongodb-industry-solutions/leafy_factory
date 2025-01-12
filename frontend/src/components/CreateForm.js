ce516a5fd99ce9a78194db24cb354919a8e724d5
Romina Carranza <romina.lopezcarranza@mongodb.com>
2025-01-11 20:04:34 -0600

    Jobs form, fix on CreateForm addWorkOrder and Navbar.js

--- frontend/src/components/CreateForm.js
+++ frontend/src/components/CreateForm.js
@@ -38,7 +38,7 @@ const CreateForm = () => {
       planned_start_date: plannedStartDate,
       planned_end_date: plannedEndDate,
       quantity: quantity,
-      wo_status: "Created",
+      status: "Created",
       //planned_cost: plannedCost,
       creation_date: creationDate,
       product_cat_id: product === "Titanium Hammer" ? 1 : product === "2 Step ladder" ? 2 : 0,
