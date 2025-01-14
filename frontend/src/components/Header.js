c8234dcf855e9d41644dbc95adc4f20a9adebd5a
Romina Carranza <romina.lopezcarranza@mongodb.com>
2025-01-14 17:59:28 -0600

    Stepper image added to Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,15 @@
-import React from 'react';
-
+import "./styles.css";
+import React from "react";
+import stepperImage from "../images/stepper.png";
 
 function Header() {
-    return (
-        <header className='header'>
-            {/*<h1>Welcome to Leafy Factory Work Order Process</h1>
-
-            <img src="https://images.contentstack.io/v3/assets/blt7151619cb9560896/blta30d0168850404a8/65fda6758f44440029c3a12a/la1a1agcxt7ppntea-logo-marks.svg" className="logo" alt="logo" />
-        */}
-            </header>
-    );
+  return (
+    <header className="header">
+      <div className="stepper">
+        <img className="stepper-img" id="stepper-img" src={stepperImage} alt="Leafy Factory Demo Architecture" />
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
