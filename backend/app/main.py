7b8774d75cbc49ec7d1cb75832aaa59d8bd281af
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2025-01-18 11:12:02 -0600

    Added library to consume the MongoDB change streams

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,8 +5,15 @@ from app.routes.products import router as products_router
 from app.routes.jobs import router as jobs_router
 from app.routes.machine_simulator import router as machines_router
 from app.routes.machines import router as machines_status
+# from app.routes.change_stream_listener import start_change_stream_listener
 
 app = FastAPI(title="Leafy Factory APIs")
+
+# Start the change stream listener when the FastAPI app starts
+# @app.on_event("startup")
+# async def startup_event():
+#     await start_change_stream_listener()
+
 
 # Configure CORS (Allow React app origin)
 app.add_middleware(
