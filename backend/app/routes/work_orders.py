eb74309a15d7f5db60d69f2a025874c073d42248
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2025-01-22 16:21:41 -0600

    Modified the implementation of the database from MariaDB to PostgreSQL

--- backend/app/routes/work_orders.py
+++ backend/app/routes/work_orders.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import JSONResponse
 from app.models.work_orders import WorkOrder, UpdateWorkOrder
-from app.database import work_orders_coll, raw_materials_coll, mariadb_conn, kfk_work_orders_coll, kfk_products_coll, kfk_product_cost_coll
+from app.database import work_orders_coll, raw_materials_coll, sql_conn, kfk_work_orders_coll, kfk_products_coll, kfk_product_cost_coll
 from pymongo.errors import DuplicateKeyError
 import datetime
 from bson.decimal128 import Decimal128
@@ -59,7 +59,7 @@ def create_work_order(work_order: WorkOrder):
                                      """
         
 
-        with mariadb_conn.cursor() as db_cur:
+        with sql_conn.cursor() as db_cur:
             # Retrieve the raw_materials_ids from the products_raw_materials table
             db_cur.execute(query_get_raw_materials_id, (product_id,))
             
@@ -109,12 +109,12 @@ def create_work_order(work_order: WorkOrder):
             item_quantity = item["quantity"]
             update_raw_materials = f"UPDATE raw_materials SET raw_material_stock = raw_material_stock - {item_quantity} WHERE item_code = %s"
             
-            with mariadb_conn.cursor() as db_cur:
+            with sql_conn.cursor() as db_cur:
                 # Update the raw material stock for that specific item_code
                 db_cur.execute(update_raw_materials, (item_code,))
 
             # Commit the changes
-            mariadb_conn.commit()
+            sql_conn.commit()
 
         #     # Right now the function doensn't check if there is enough stock
         #     # TODO validate that there is enough stock before creating the work order.
@@ -129,7 +129,8 @@ def create_work_order(work_order: WorkOrder):
                                 creation_date, 
                                 product_id
                             )
-                            VALUES (?,?,?,?,?,?)
+                            VALUES (%s, %s, %s, %s, %s, %s)
+                            RETURNING id_work;
                             """
         
         insert_cost_work_order ="""
@@ -140,10 +141,10 @@ def create_work_order(work_order: WorkOrder):
                                     total_cost_per_wo,
                                     work_id
                                 )
-                                VALUES (?,?,?,?,?)
+                                VALUES (%s, %s, %s, %s, %s);
                                 """
         
-        with mariadb_conn.cursor() as db_insert_cursor:
+        with sql_conn.cursor() as db_insert_cursor:
             db_insert_cursor.execute(insert_work_order, 
                                      (
                                         workorder_json["planned_start_date"],
@@ -154,7 +155,8 @@ def create_work_order(work_order: WorkOrder):
                                         workorder_json["product_cat_id"],
                                      ))
             
-            new_work_order_id = db_insert_cursor.lastrowid
+            new_work_order_id = db_insert_cursor.fetchone()[0]
+            print(new_work_order_id)
 
             db_insert_cursor.execute(insert_cost_work_order, 
                                      (
@@ -164,11 +166,9 @@ def create_work_order(work_order: WorkOrder):
                                          workorder_json["cost"]["planned"]["total_cost_per_wo"],
                                          new_work_order_id,
                                      ))
-            
-            new_work_cost_id = db_insert_cursor.lastrowid
 
             # Commit the insert operation.
-            mariadb_conn.commit()
+            sql_conn.commit()
         
         return JSONResponse(
             status_code=status.HTTP_201_CREATED,
@@ -181,15 +181,13 @@ def create_work_order(work_order: WorkOrder):
             detail="A work order with this work_id already exists"
         )
     except Exception as e:
-        mariadb_conn.rollback()
+        sql_conn.rollback()
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Failed to create work order: {str(e)}"
         )
 
 
-# Getting the data from the kafka collections
-# TODO
 # This API should get the data from MongoDB, the SQL data needs to be inserted into MongoDB to do so.
 # Get the last 100 work orders
 @router.get("/workorders/",
@@ -224,44 +222,47 @@ def create_work_order(work_order: WorkOrder):
             })
 def get_work_order():
     work_order_list= []
-    work_order_cursor = kfk_work_orders_coll.find({},).limit(100).sort({"creation_date": -1})
+    try:
+        work_order_cursor = kfk_work_orders_coll.find({},).limit(100).sort({"creation_date": -1})
 
-    for workorder_item in work_order_cursor:
-        
-        product_item = kfk_products_coll.find_one({"id_product" : workorder_item["product_id"]})
-        total_cost_wo = kfk_product_cost_coll.find_one({"work_id": workorder_item["id_work"]})
-        
-        workorder_item["product_name"] = product_item["product_name"]
-        workorder_item["planned_cost"] = str(total_cost_wo["total_cost_per_wo"])
-        # workorder_item["planned_cost"] = 0
-        workorder_item["actual_cost"] = float(total_cost_wo["actual_total_cost"].to_decimal())if total_cost_wo["actual_total_cost"] != None else None
-        # workorder_item["actual_cost"] = 0
-
-        # Change datetime format from epoch to timestamp
-        workorder_item["planned_start_date"] = datetime.datetime.fromtimestamp(workorder_item["planned_start_date"]/1000)
-        workorder_item["planned_end_date"] = datetime.datetime.fromtimestamp(workorder_item["planned_end_date"]/1000)
-        workorder_item["creation_date"] = datetime.datetime.fromtimestamp(workorder_item["creation_date"]/1000)
-
-        
-        # In case the actual_start_date and actual_end_date is not set
-        if workorder_item["actual_start_date"] != None:
-            workorder_item["actual_start_date"] = datetime.datetime.fromtimestamp(workorder_item["actual_start_date"]/1000)
-
-        if workorder_item["actual_end_date"] != None:
-            workorder_item["actual_end_date"] = datetime.datetime.fromtimestamp(workorder_item["actual_end_date"]/1000)
+        for workorder_item in work_order_cursor:
             
+            product_item = kfk_products_coll.find_one({"id_product" : workorder_item["product_id"]})
+            total_cost_wo = kfk_product_cost_coll.find_one({"work_id": workorder_item["id_work"]})
+            
+            workorder_item["product_name"] = product_item["product_name"]
+            workorder_item["planned_cost"] = str(total_cost_wo["total_cost_per_wo"])
+            # workorder_item["planned_cost"] = 0
+            workorder_item["actual_cost"] = float(total_cost_wo["actual_total_cost"].to_decimal())if total_cost_wo["actual_total_cost"] != None else None
+            # workorder_item["actual_cost"] = 0
 
-        work_order_list.append(workorder_item)
+            # Change datetime format from epoch to timestamp
+            workorder_item["planned_start_date"] = datetime.datetime.fromtimestamp(workorder_item["planned_start_date"]/1000)
+            workorder_item["planned_end_date"] = datetime.datetime.fromtimestamp(workorder_item["planned_end_date"]/1000)
+            workorder_item["creation_date"] = datetime.datetime.fromtimestamp(workorder_item["creation_date"]/1000)
+
+            
+            # In case the actual_start_date and actual_end_date is not set
+            if workorder_item["actual_start_date"] != None:
+                workorder_item["actual_start_date"] = datetime.datetime.fromtimestamp(workorder_item["actual_start_date"]/1000)
+
+            if workorder_item["actual_end_date"] != None:
+                workorder_item["actual_end_date"] = datetime.datetime.fromtimestamp(workorder_item["actual_end_date"]/1000)
+                
+
+            work_order_list.append(workorder_item)
 
 
-    if not work_order_list:
+        if not work_order_list:
+            work_order_list = []
+        
+        #Returns a list of JSON documents (work_order_list)
+        return work_order_list
+    except Exception as e:
         raise HTTPException (
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Work orders not found"
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error retrieving work orders"
         )
-    
-    #Returns a list of JSON documents (work_order_list)
-    return work_order_list
 
 
 #Update work order from Created to Completed
@@ -288,30 +289,34 @@ def update_work_order(work_id: int, updated_work_order: UpdateWorkOrder):
         updated_workorder_json = updated_work_order.model_dump()
         work_order_status = "Completed"
 
-        # Update the document, adding actual_start_date, actual_end_date and actual.final_product_cost_per_job
-        work_order_update_query = f"UPDATE work_orders SET actual_start_date = %s, actual_end_date = %s, wo_status = %s, nOk_products = %s WHERE id_work = {work_id}"
-        cost_update_query = f"UPDATE product_cost SET cost_ok_with_overhead = %s, cost_nok_with_overhead = %s, actual_total_cost = %s WHERE work_id = {work_id}"
+        # Update the document, adding actual_start_date, actual_end_date and final_product_cost_per_job
+        work_order_update_query = "UPDATE work_orders SET actual_start_date = %s, actual_end_date = %s, wo_status = %s, nOk_products = %s WHERE id_work = %s"
+        cost_update_query = "UPDATE product_cost SET cost_ok_with_overhead = %s, cost_nok_with_overhead = %s, actual_total_cost = %s WHERE work_id = %s"
 
-        with mariadb_conn.cursor() as db_cur:
+        with sql_conn.cursor() as db_cur:
             db_cur.execute(work_order_update_query, (
                 updated_workorder_json["actual_start_date"],
                 updated_workorder_json["actual_end_date"],
                 work_order_status,
-                updated_workorder_json['final_product_cost_per_job']['nOk_products']
+                updated_workorder_json['final_product_cost_per_job']['nOk_products'],
+                work_id
             ),)
 
+            # Get number of rows affected by the work order update
             total_wo_updated = db_cur.rowcount
 
             db_cur.execute(cost_update_query, (
                 updated_workorder_json['final_product_cost_per_job']['cost_ok_with_overhead'],
                 updated_workorder_json['final_product_cost_per_job']['cost_nok_with_overhead'],
                 updated_workorder_json['final_product_cost_per_job']['total_cost'],
+                work_id
             ),)
 
+            # Get number of rows affected by the cost update
             total_product_cost_updated = db_cur.rowcount
             
             # Commit the update operations.
-            mariadb_conn.commit()
+            sql_conn.commit()
 
         return JSONResponse(
             status_code=status.HTTP_200_OK,
