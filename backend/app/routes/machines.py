31e283fd5603f1b4e0ee97b3dba16edab8e23247
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2025-01-23 12:06:05 -0600

    Removed the function that originally sent data to lambda

--- backend/app/routes/machines.py
+++ backend/app/routes/machines.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, status
-from app.database import machine_data_coll, raw_sensor_data_coll, factories_data_coll, kfk_machines_coll
+from app.database import raw_sensor_data_coll, factories_data_coll, kfk_machines_coll
 from app.models.machines import MachineHeartbeat, MachineValue
 from fastapi.responses import JSONResponse
 from datetime import datetime, timedelta
@@ -145,75 +145,3 @@ async def retrieve_machine_details():
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to retrieve machine details: {str(e)}")
-
-
-@router.post("/machines/heartbeat")
-async def receive_heartbeat(data: MachineHeartbeat):
-    """This endpoint inserts the heartbeat data sent from Lambda to MongoDB database using bucket pattern"""
-    try:
-        heartbeat_record = {
-            "factory_id": data.factory_id,
-            "production_line_id": data.production_line_id,
-            "machine_id": data.machine_id,
-            "vibration": data.vibration,
-            "temperature": data.temperature,
-            "timestamp": data.timestamp
-        }
-
-        print(heartbeat_record['timestamp'])
-        
-        # list_time[0] includes start_date
-        # list_time[1] includes end_date
-        list_time = get_bucket_time(heartbeat_record['timestamp'])
-
-        # Bucket filter query
-        filter_query = {
-            "start_date": list_time[0],
-            "end_date": list_time[1],
-            "factory.factory_id": heartbeat_record["factory_id"],
-            "factory.production_lines.machine.machine_id": heartbeat_record["machine_id"]
-        }
-
-        # Define the update operation
-        update_query = {
-            "$push": {
-                "measurements": {
-                    "timestamp": heartbeat_record["timestamp"],
-                    "temperature": heartbeat_record["temperature"],
-                    "vibration": heartbeat_record["vibration"]
-                }
-            },
-            "$inc": {
-                "transaction_count": 1,
-                "sum_temperature": heartbeat_record["temperature"],
-                "sum_vibration": heartbeat_record["vibration"]
-            },
-            "$setOnInsert": {
-                "start_date": list_time[0],
-                "end_date": list_time[1],
-                "factory": {
-                    "factory_id": heartbeat_record["factory_id"],
-                    "production_lines": [
-                        {
-                            "production_line_id": heartbeat_record["production_line_id"],
-                            "machine": {
-                                "machine_id": heartbeat_record["machine_id"]
-                            }
-                        }
-                    ]
-                }
-            }
-        }
-
-        # Insert or update the bucket
-        update_result = machine_data_coll.update_one(filter_query, update_query, upsert=True)
-        print(f"Upserted ID: {update_result.upserted_id}")
-        print(f"Result: {update_result.acknowledged}")
-
-        return JSONResponse(
-            status_code=status.HTTP_201_CREATED,
-            content={"machine_data_id": str(r"test_123")}
-        )
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Failed to save heartbeat: {str(e)}")
