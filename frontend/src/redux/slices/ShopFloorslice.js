2a817b5d4781418b63d4341e99cd04eac73ecb8c
romibernard <romina.carranza@mongodb.com>
2025-03-27 15:52:12 -0600

    Tabs addition and change streams declaration in FE

--- frontend/src/redux/slices/ShopFloorslice.js
+++ frontend/src/redux/slices/ShopFloorslice.js
@@ -5,6 +5,7 @@ const shopfloorSlice = createSlice({
   initialState: {
     isRunning: false,
     error: null,
+    sensorData: [],
     machineDetails: null,
   },
   reducers: {
@@ -19,6 +20,9 @@ const shopfloorSlice = createSlice({
     shopfloorFailure: (state, action) => {
       state.error = action.payload;
     },
+    addSensorData: (state, action) => {
+      state.sensorData = [...state.sensorData, action.payload]
+    },
     setMachineDetails: (state, action) => {
       state.machineDetails = action.payload;
     },
@@ -32,6 +36,7 @@ export const {
   startShopfloor,
   stopShopfloor,
   shopfloorFailure,
+  addSensorData,
   setMachineDetails,
   setMachineDetailsError,
 } = shopfloorSlice.actions;
